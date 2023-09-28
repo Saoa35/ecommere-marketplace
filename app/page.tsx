90ac29df7839dd90eef436c41021d57cc9b28fd5
Saoa35 <saoamah@gmail.com>
2023-09-28 09:35:17 +0400

    + created typing file

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <main className="">HOME</main>;
+export default async function Home() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
+
+  // console.log(products);
+
+  return (
+    <main className="">
+      <section className="flex flex-col space-y-12 pb-44">
+        <h1 className=" text-5xl font-bold text-center">DEALS OF THE DAY</h1>
+
+        {products.map((product) => (
+          <div key={product.id}>
+            <h1>{product.title}</h1>
+          </div>
+        ))}
+
+        <div className=""></div>
+      </section>
+    </main>
+  );
 }
