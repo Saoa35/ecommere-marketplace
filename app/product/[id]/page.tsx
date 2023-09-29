2e67186eadefeaf47794eb3b732cc450bb76bb68
Saoa35 <saoamah@gmail.com>
2023-09-29 11:27:59 +0400

    + created ProductPageProps types

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,10 @@
-export default function ProductPage(props) {
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function ProductPage(props: ProductPageProps) {
   console.log(props);
 
   return <main className="">ProductPage</main>;
