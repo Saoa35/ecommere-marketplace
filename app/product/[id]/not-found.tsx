b26cbbe4515547955c92f2d17d9f69f272fb5eac
Saoa35 <saoamah@gmail.com>
2023-09-30 09:48:45 +0400

    + installed headlessui

--- app/product/[id]/not-found.tsx
+++ app/product/[id]/not-found.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="mt-28 flex flex-col items-center gap-2">
+      <h2 className=" font-bold">Not Found</h2>
+      <p className=" font-semibold">Could not find requested resource</p>
+      <Link
+        href="/"
+        className="border border-red-600 bg-red-100 px-2 py-1 rounded-xl hover:text-white hover:bg-slate-700 hover:border-none"
+      >
+        Return Home
+      </Link>
     </div>
   );
 }
