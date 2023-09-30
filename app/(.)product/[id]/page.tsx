5b60a9590cd67fdc6d25aa88bdc4234f74870f92
Saoa35 <saoamah@gmail.com>
2023-09-30 12:21:17 +0400

    + Dialog to Modal

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { ProductImage } from "@/components/ProductImage";
 import { Dialog } from "@headlessui/react";
-import { useParams } from "next/navigation";
+import { StarIcon as StarIconOutline } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Modal() {
   const [isOpen, setIsOpen] = useState(true);
   const [product, setProduct] = useState<Product>();
+  const router = useRouter();
 
   const id = useParams().id;
 
@@ -14,7 +18,8 @@ export default function Modal() {
     async function fetchProduct() {
       const res = await fetch(`https://fakestoreapi.com/products/${id}`);
       const product = await res.json();
-      console.log(product);
+
+      setProduct(product);
     }
 
     fetchProduct();
@@ -29,7 +34,67 @@ export default function Modal() {
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <Dialog.Panel className="mx-auto max-w-3xl rounded bg-white p-10"></Dialog.Panel>
+        <Dialog.Panel className="mx-auto max-w-3xl rounded bg-white p-10">
+          <div className="flex gap-x-8 h-96">
+            {product?.image && (
+              <div className="relative w-72 h-full hidden md:inline">
+                <ProductImage product={product} fill />
+              </div>
+            )}
+
+            <div className="flex-1 flex flex-col">
+              <div className="flex-1">
+                <h4 className=" font-semibold">{product?.title}</h4>
+                <p className=" font-medium text-sm">${product?.price}</p>
+
+                <div className="flex items-center text-sm my-4">
+                  <p>{product?.rating.rate}</p>
+                  {product?.rating.rate && (
+                    <div className="flex items-center ml-2 mr-6">
+                      {Array.from(
+                        { length: Math.floor(product.rating.rate) },
+                        (_, i) => (
+                          <StarIcon
+                            key={i}
+                            className="h-4 w-4 text-yellow-500"
+                          />
+                        )
+                      )}
+
+                      {Array.from(
+                        { length: 5 - Math.floor(product.rating.rate) },
+                        (_, i) => (
+                          <StarIconOutline
+                            key={i}
+                            className="h-4 w-4 text-yellow-500"
+                          />
+                        )
+                      )}
+                    </div>
+                  )}
+
+                  <p className="text-blue-600 hover:underline cursor-pointer text-xs">
+                    See all {product?.rating.count} reviews
+                  </p>
+                </div>
+
+                <p className="line-clamp-5 text-sm">{product?.description}</p>
+              </div>
+
+              <div className=" space-y-3 text-sm">
+                <button className="button w-full bg-green-600 text-white border-transparent hover:border-green-600 hover:bg-transparent hover:text-black">
+                  Add to bag
+                </button>
+                <button
+                  // onClick={() => router.push(`/product/${product?.id}`)}
+                  className="button w-full bg-transparent border-green-600 hover:bg-green-600  hover:text-white hover:border-transparent"
+                >
+                  View full details
+                </button>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
       </div>
     </Dialog>
   );
