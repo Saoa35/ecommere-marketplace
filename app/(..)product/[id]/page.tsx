c1fdccb791bb64ecd7e44dddbba2a26eccdd91ac
Saoa35 <saoamah@gmail.com>
2023-09-30 09:56:30 +0400

    + created Modal component

--- app/(..)product/[id]/page.tsx
+++ app/(..)product/[id]/page.tsx
@@ -1,39 +1,9 @@
-import { ProductImage } from "@/components/ProductImage";
-import { notFound } from "next/navigation";
+"use client";
 
-type ProductPageProps = {
-  params: {
-    id: string;
-  };
-};
+import { useState } from "react";
 
-export default async function ProductPage({
-  params: { id },
-}: ProductPageProps) {
-  try {
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+export default function Modal() {
+  const [isOpen, setIsOpen] = useState(true);
 
-    const product: Product = await res.json();
-
-    return (
-      <div className=" max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10">
-        <ProductImage product={product} />
-
-        <div className=" divide-y">
-          <div className=" space-y-2 pb-8">
-            <h1 className="text-2xl md:text-4xl font-bold">{product.title}</h1>
-            <h2 className=" text-gray-500 font-bold text-xl md:text-3xl">
-              ${product.price}
-            </h2>
-          </div>
-
-          <div className="pt-8">
-            <p className="text-xs md:text-sm">{product.description}</p>
-          </div>
-        </div>
-      </div>
-    );
-  } catch (error) {
-    notFound();
-  }
+  return <div>Modal</div>;
 }
